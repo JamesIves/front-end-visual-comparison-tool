45338e853e32fcceaac29fb7def453c120672300
James Ives <iam@jamesiv.es>
2018-07-12 11:12:33 -0500

    WIP

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import './App.css';
 import Diff from './Diff';
 import Original from './Original';
+import Navigation from './Navigation';
+import Filter from './Filter';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tests: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api.json').then((response) => {
+      return response.json()
+    }).then((response) => {
+      this.setState({
+        tests: response
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.tests)
     return (
       <div className="App">
-        <Original src='http://localhost:3000/diff/homepage/728/live.png' />
-        <Diff src='http://localhost:3000/diff/homepage/728/dev.png' overlay='http://localhost:3000/diff/homepage/728/diff.png' />
+        <Navigation items={this.state.tests} >
+          <Filter>
+            <Original src='http://localhost:3000/diff/homepage/728/live.png' />
+            <Diff src='http://localhost:3000/diff/homepage/728/dev.png' overlay='http://localhost:3000/diff/homepage/728/diff.png' />   
+          </Filter>
+        </Navigation>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+function startServer() {
+  app.listen(9000);
+}
