71229e3b50d59ecf4ec0b655bbfb5e6f782db82a
James Ives <iam@jamesiv.es>
2018-07-16 15:18:44 -0500

    Adding Proper Buttons

--- client/src/components/TestShow.js
+++ client/src/components/TestShow.js
@@ -4,7 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { fetchTest, removeTest, runTest } from '../actions/index';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -19,6 +21,21 @@ const styles = theme => ({
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
+  },
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   }
 });
 
@@ -74,24 +91,31 @@ class TestShow extends Component {
         <div>{this.state.status}</div>
         <Grid container spacing={24}>
           <Grid item xs={12}>
-            <Paper className={classes.root} elevation={1}>
-            <Typography variant="headline" component="h3">
-              {test.name}
-            </Typography>
-            <Typography component="p">
-              Showing a visual comparrison between the two pages. Clicking the image on the right side will toggle
-              a pixel comparison overlay that you can use to highlight changes.
-            </Typography>
-            <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Edit the test if you've made a mistake.">
-              <Button onClick={this.onEditClick}>Edit Test</Button>
-            </Tooltip>
-            <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Once selected the test will run in the background, the page may refresh a few times.">
-              <Button onClick={this.onRunClick}>Run Test</Button>
-            </Tooltip>
-            <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Removes the test from the database.">
-              <Button onClick={this.onRemoveClick}>Remove Test</Button>
-            </Tooltip>
-            </Paper>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="headline" component="h2">
+                  {test.name}
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  {test.description}
+                </Typography>
+                <Typography component="p">
+                Showing a visual comparrison between the two pages. Clicking the image on the right side will toggle
+                a pixel comparison overlay that you can use to highlight changes.
+                </Typography>
+              </CardContent>
+              <CardActions>
+              <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Edit the test if you've made a mistake.">
+                <Button onClick={this.onEditClick}>Edit Test</Button>
+                </Tooltip>
+                <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Once selected the test will run in the background, the page may refresh a few times.">
+                  <Button onClick={this.onRunClick}>Run Test</Button>
+                </Tooltip>
+                <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }} title="Removes the test from the database.">
+                  <Button onClick={this.onRemoveClick}>Remove Test</Button>
+                </Tooltip>
+              </CardActions>
+            </Card>
 
             {this.state.running ?
               <LinearProgress />
