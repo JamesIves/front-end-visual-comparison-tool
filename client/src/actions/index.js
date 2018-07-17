075043dca23abd83a671771b571ca2332a0782aa
James Ives <iam@jamesiv.es>
2018-07-16 20:29:59 -0500

    General Cleanup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,11 +2,9 @@ import axios from 'axios';
 import {
   FETCH_TEST,
   FETCH_TESTS,
-  FETCH_TEST_SIZE,
   RUN_TEST,
   ADD_TEST,
   REMOVE_TEST,
-  REMOVE_SIZE
 } from './types';
 
 export const ROOT_URL = 'http://localhost:9090';
@@ -32,7 +30,7 @@ export function fetchTest(id) {
 
 export function runTest(id) {
   const request = axios.get(`${ROOT_URL}/run/${id}`)
-  console.log('Running the test', request)
+
   return {
     type: RUN_TEST,
     payload: request
@@ -41,6 +39,7 @@ export function runTest(id) {
 
 export function addTest(props) {
   const request = axios.post(`${ROOT_URL}/tests`, props)
+
   return {
     type: ADD_TEST,
     payload: request
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,5 @@
 export const FETCH_TEST = 'fetch_test';
 export const FETCH_TESTS = 'fetch_tests';
-export const FETCH_TEST_SIZE = 'fetch_test_size';
 export const ADD_TEST = 'add_test';
 export const REMOVE_TEST = 'remove_test';
-export const REMOVE_SIZE = 'remove_size';
 export const RUN_TEST = 'run_test';
