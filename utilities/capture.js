14a3fdd932144104bd895b713c9f0ae4b43df31e
James Ives <iam@jamesiv.es>
2018-07-23 15:43:06 -0500

    Semi Colons

--- utilities/capture.js
+++ utilities/capture.js
@@ -38,16 +38,16 @@ async function runTest(test) {
         width: size,
         height: 0
       })
-    }
+    };
 
     /* Navitates puppeteer to the page to take the screenshot. If an error is
     encountered we throw an exception cancelling the rest of the process.
     These errors are collected in an API response so we can alert the front-end. */
     await page.goto(live).catch((error) => {
       throw `${error} ${live}`
-    })
+    });
 
-    log(chalk.yellow(`Taking Live screenshot for the ${name} test!`))
+    log(chalk.yellow(`Taking Live screenshot for the ${name} test!`));
       
     /* We set a timeout here to make sure that all initial load animations
     have finished playing before we take the screenshots. */
@@ -60,7 +60,7 @@ async function runTest(test) {
 
     await page.goto(dev).catch((error) => {
       throw `${error} ${dev}`
-    })
+    });
 
     log(chalk.yellow(`Taking Dev screenshot for the ${name} test!`));
     await timeout(5000);
@@ -70,24 +70,24 @@ async function runTest(test) {
       fullPage: true
     });
       
-    log(chalk.green(`Generating diff overlay...`))
+    log(chalk.green(`Generating diff overlay...`));
 
     /* Once we have both of our screenshots we send them over to the compare function
     so we can get a pixel overlay for each image. */
-    await compare(_id)
+    await compare(_id);
 
   } catch(error) {
-    log(chalk.red(`Encountered an error while taking a screenshot: ${error}`))
-    await browser.close()
+    log(chalk.red(`Encountered an error while taking a screenshot: ${error}`));
+    await browser.close();
 
     test['success'] = false;
-    return test
+    return test;
 
   } finally {
-    await browser.close()
+    await browser.close();
 
     test['success'] = true;
-    return test
+    return test;
   }
 }
 
@@ -100,18 +100,18 @@ module.exports = (tests) => {
   return new Promise((resolve, reject) => {
     if (!fs.existsSync(directory)) {
       mkdirp(directory, (() => {
-        log(chalk.red(`Created directory: ${directory}`))
-      }))
-    }
+        log(chalk.red(`Created directory: ${directory}`));
+      }));
+    };
   
     let testPromises = tests.map((test) =>  {
-      return runTest(test)
+      return runTest(test);
     });
   
     Promise.all(testPromises).then((results) => {
-      resolve(results)
+      resolve(results);
     }).catch(() => {
-      reject('Encountered processing error.')
-    })
+      reject('Encountered processing error.');
+    });
   })
 }
